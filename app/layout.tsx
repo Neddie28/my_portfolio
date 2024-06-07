f6c9d884d94c8250c14371767df9fdd0ce3e7fe7
Neddie28 <Neddiecornel001@gmail.com>
2024-06-07 22:19:53 +0100

    another commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
 <link rel="icon" type="image/svg+xml" href="/logo.svg" />
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +19,9 @@ export default function RootLayout({
  
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+            {children}
+        </body>
     </html>
   )
 }
